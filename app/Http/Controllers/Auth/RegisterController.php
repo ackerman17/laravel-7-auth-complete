be8b10be8bec14f081dc8ddb4768ef76fc4edd07
mhafet17 <do.crazy192@gmail.com>
2020-08-29 20:41:23 +0700

    tambah validasi registercontroller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,6 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
+            'username' => ['alpha_num', 'required', 'string', 'min:3', 'max:100', 'unique:users'],
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
@@ -65,9 +66,16 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
+            'username' => $data['username'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    protected function registered()
+    {
+        $this->guard()->logout();
+        return redirect()->route('login')->with(['success' => 'You are register and please verify email address']);
     }
 }
